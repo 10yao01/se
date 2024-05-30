cf565c03e402d5690e085297cf13ec5aaefacc69
张正博 <3506969946@qq.com>
2024-05-30 12:09:54 +0800

    更新5.28

--- BackEnd/src/main/java/com/example/farm_management/mapper/FarmMapper.java
+++ BackEnd/src/main/java/com/example/farm_management/mapper/FarmMapper.java
@@ -14,7 +14,7 @@ public interface FarmMapper {
     List<Farm> list(String fname, String fid, String posinfo, String cname, String cid);
 
     @Insert("insert into farm(f_id,f_name,c_id,c_name,c_amount,p_date,w_id,harvest_date,stage,pos_info)" +
-            "values (#{fid},#{fname},#{cid},#{cname},#{camount},#{wid},#{pdate},#{harvestdate},#{stage},#{posinfo})")
+            "values (#{fid},#{fname},#{cid},#{cname},#{camount},#{pdate},#{wid},#{harvestdate},#{stage},#{posinfo})")
     void insert(Farm farm);
 
     void delete(List<Integer> ids);
